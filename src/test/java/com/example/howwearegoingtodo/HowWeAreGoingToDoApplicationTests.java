0e0cbf38f191b18ff0c065247d09f9d04800203e
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-08-25 15:12:18 +0200

    With H2 Database only

--- src/test/java/com/example/howwearegoingtodo/HowWeAreGoingToDoApplicationTests.java
+++ src/test/java/com/example/howwearegoingtodo/HowWeAreGoingToDoApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.howwearegoingtodo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-/*
+
 @SpringBootTest
 class HowWeAreGoingToDoApplicationTests {
 
@@ -11,4 +11,3 @@ class HowWeAreGoingToDoApplicationTests {
     }
 
 }
-*/
